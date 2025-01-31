1d558de7ca583288e6319b350c4597b9ab0c850c
Edward <ayvari16@gmail.com>
2025-01-30 22:01:36 -0500

    Fixed some UI issues with adding steps and ingredients, updated recipe model

--- mywebsite/recipes/models.py
+++ mywebsite/recipes/models.py
@@ -49,7 +49,7 @@ class Ingredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
     name = models.CharField(max_length=70)
     quantity = models.CharField(max_length=20, null=True, blank=True)
-    measurement = models.CharField(max_length=50, choices=MEASUREMENT_CHOICES, null=True, blank=True, default='cup')
+    measurement = models.CharField(max_length=50, choices=MEASUREMENT_CHOICES, null=True, blank=True)
 
     def __str__(self) -> str:
         quantity_display = f"{self.quantity} " if self.quantity else ""
