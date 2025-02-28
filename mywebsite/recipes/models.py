bce8a7fc9373170c1ba2b3022c0e1c30577da58b
Edward <ayvari16@gmail.com>
2025-02-27 22:04:23 -0500

    updated models, added separate steps and ingredients models for recipe and subrecipe

--- mywebsite/recipes/models.py
+++ mywebsite/recipes/models.py
@@ -8,42 +8,48 @@ from django.dispatch import receiver
 import os
 
 MEASUREMENT_CHOICES = [
-        ('cup', 'Cup'),
-        ('gram', 'Gram'),
-        ('tbs', 'Tablespoon'),
-        ('tsp', 'Teaspoon'),
-        ('Milliliter', 'Milliliter'),
-        ('liter', 'Liter'),
-        ('Pound', 'Pound'),
-        ('bag', 'Bag'),
-        ('piece(s)', 'Piece(s)'),
-        ('slice(s)', 'Slices')
-    ]
+    ('cup', 'Cup'),
+    ('gram', 'Gram'),
+    ('tbs', 'Tablespoon'),
+    ('tsp', 'Teaspoon'),
+    ('Milliliter', 'Milliliter'),
+    ('liter', 'Liter'),
+    ('Pound', 'Pound'),
+    ('bag', 'Bag'),
+    ('piece(s)', 'Piece(s)'),
+    ('slice(s)', 'Slices')
+]
 
-class Recipe(models.Model):
+
+class BaseRecipe(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    note = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    picture = models.ImageField(upload_to='recipes_pictures/', blank=True, null=True)
-    is_subrecipe = models.BooleanField(default=False)
-    related_sub_recipes = models.ManyToManyField('self', through='RecipeSubRecipe',
-                                        through_fields=('recipe', 'sub_recipe'),
-                                        symmetrical=False, related_name='sub_recipes')
-    
+    note = models.TextField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.pk})
-    
-    def __str__(self):
-        return self.title
-    
+
+
+class Recipe(BaseRecipe):
+    description = models.TextField(blank=True, null=True)
+    picture = models.ImageField(upload_to='recipes_pictures/', blank=True, null=True)
+
+
+class SubRecipe(BaseRecipe):
+    recipes = models.ManyToManyField(Recipe, through='RecipeSubRecipe')
+
 
 class RecipeSubRecipe(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='parent_recipe')
-    sub_recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='used_sub_recipes')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='linked_recipes')
+    sub_recipe = models.ForeignKey(SubRecipe, on_delete=models.CASCADE, related_name='linked_sub_recipes')
     specific_notes = models.TextField(blank=True, null=True)
     sub_recipe_quantity = models.CharField(max_length=20, null=True, blank=True)
     sub_recipe_measurement = models.CharField(max_length=50, choices=MEASUREMENT_CHOICES, null=True, blank=True)
@@ -54,31 +60,51 @@ class RecipeSubRecipe(models.Model):
         ]
 
 
-
 class Ingredient(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
     name = models.CharField(max_length=70)
     quantity = models.CharField(max_length=20, null=True, blank=True)
     measurement = models.CharField(max_length=50, choices=MEASUREMENT_CHOICES, null=True, blank=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self) -> str:
         quantity_display = f'{self.quantity}' if self.quantity else ''
         measurement_display = f'{self.measurement}' if self.measurement else ''
         return f'{quantity_display}{measurement_display}{self.name}'.strip()
-    
+
+
+class RecipeIngredient(Ingredient):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+
+
+class SubRecipeIngredient(Ingredient):
+    sub_recipe = models.ForeignKey(SubRecipe, on_delete=models.CASCADE, related_name='ingredients')
+
 
 class Step(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='steps')
     order = models.PositiveIntegerField()
     description = models.TextField()
 
+    class Meta:
+        abstract = True
+        ordering = ['order']
 
     def __str__(self) -> str:
         return f'Step {self.order} {self.description}'
 
+
+class RecipeStep(Step):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='steps')
+
+
+class SubRecipeStep(Step):
+    sub_recipe = models.ForeignKey(SubRecipe, on_delete=models.CASCADE, related_name='sub_steps')
+
+
 # Signal handler to delete the image file before the model instance is deleted
 @receiver(pre_delete, sender=Recipe)
 def delete_image_on_delete_model(sender, instance, **kwargs):
-        if instance.picture:
-            if os.path.isfile(instance.picture.path):
-                os.remove(instance.picture.path)
+    if instance.picture:
+        if os.path.isfile(instance.picture.path):
+            os.remove(instance.picture.path)
