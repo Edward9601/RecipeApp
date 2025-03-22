9970aa66c4d191d55f4207846ca80885e76e3c15
Edward <ayvari16@gmail.com>
2025-03-22 17:49:41 -0400

    Updated search functionality to be able to filter by ingredient that recipe contains

--- mywebsite/recipes/views.py
+++ mywebsite/recipes/views.py
@@ -35,12 +35,19 @@ class RecipeListView(BaseRecipeView, ListView):
 
     def get(self, request, *args, **kwargs):
         if request.htmx:
-            return self.get_recipes(request)
+            return self.search(request)
         return super().get(request, *args, **kwargs)
 
-    def get_recipes(self, request):
+    def search(self, request):
         search = request.GET.get('search_text')
-        recipes = self.model.objects.filter(title__icontains=search) if search else self.model.objects.all()
+        search_type = request.GET.get('searchType')
+        if search:
+            if search_type.lower() == 'title':
+                recipes = self.model.objects.filter(title__icontains=search)
+            else:
+                recipes = self.model.objects.filter(ingredients__name__icontains=search).distinct()
+        else:
+            recipes = self.model.objects.all()
         return render(request, 'recipes/partials/recipe_list.html', {'recipes': recipes})
 
 
@@ -51,8 +58,9 @@ class RecipeDetailView(BaseRecipeView, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        self.object = self.model.objects.prefetch_related('steps','ingredients', 'sub_recipes' ).get(pk=self.object.pk)
+        self.object = self.model.objects.prefetch_related('steps','ingredients', 'sub_recipes').get(pk=self.object.pk)
         return context
+
 
 
 class BaseViewForDataUpdate(BaseRecipeView):
@@ -174,13 +182,20 @@ class SubRecipeListView(BaseSubRecipeView, ListView):
 
     def get(self, request, *args, **kwargs):
         if request.htmx:
-            return self.search_subrecipes(request)
+            return self.search(request)
         else:
             return super().get(request, *args, **kwargs)
 
-    def search_subrecipes(self, request):
+    def search(self, request):
         search = request.GET.get('search_text')
-        sub_recipes = self.model.objects.filter(title__icontains=search) if search else self.model.objects.all()
+        search_type = request.GET.get('searchType')
+        if search:
+            if search_type.lower() == 'title':
+                sub_recipes = self.model.objects.filter(title__icontains=search)
+            else:
+                sub_recipes = self.model.objects.filter(sub_ingredients__name__icontains=search).distinct()
+        else:
+            sub_recipes = self.model.objects.all()
         return render(request, 'recipes/partials/sub_recipe_list.html', {'sub_recipes': sub_recipes})
 
 
@@ -225,7 +240,7 @@ class SubRecipeDetailView(BaseSubRecipeView, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        self.object = self.model.objects.prefetch_related('sub_ingredients', 'sub_steps')
+        self.object = self.model.objects.prefetch_related('sub_ingredients', 'sub_steps', 'main_recipes')
 
         return context
 
@@ -255,20 +270,18 @@ class SubRecipeUpdateView(BaseSubRecipeView, UpdateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        self.object = form.save()
+        self.object = form.save(commit=False)
 
         context = self.get_context_data()
         ingredient_formset = context['ingredient_formset']
-        if ingredient_formset.is_valid():
-            ingredient_formset.save()
-        else:
-            return self.form_invalid(form)
         steps_formset = context['step_formset']
-        if steps_formset.is_valid():
+
+        if ingredient_formset.is_valid() or steps_formset.is_valid():
+            ingredient_formset.save()
             steps_formset.save()
+            form.save()
         else:
             return self.form_invalid(form)
-
         return super().form_valid(form)
 
 
