95f090e36d03b7f84cf877ea3e35abec051bc2be
Edward <ayvari16@gmail.com>
2025-10-26 23:04:12 -0400

    cleaned test for recipe handler

--- recipes/tests/test_recipe_handler.py
+++ recipes/tests/test_recipe_handler.py
@@ -1,13 +1,4 @@
 from .base import BaseTestCase
-from recipes.models.recipe_models import Recipe, Category, Tag, RecipeImage, RecipeIngredient, RecipeStep, SubRecipe
-from recipes.handlers import recipes_handler
-from recipes.forms.recipe_forms import RecipeImageForm, RecipeCreateForm
-from PIL import Image
-import tempfile
-
-from django.test import RequestFactory
-from django.db.models.query import QuerySet
-from django.core.files.uploadedfile import SimpleUploadedFile
 
 class RecipeHandlerTestCase(BaseTestCase):
     
